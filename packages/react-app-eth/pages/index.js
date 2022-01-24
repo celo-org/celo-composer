a9ad756e6bd6e320613c0807d1ce4662027e90ab
Josh Crites <critesjosh@gmail.com>
2022-01-24 13:12:05 -0500

    style + notifications

--- packages/react-app-eth/pages/index.js
+++ packages/react-app-eth/pages/index.js
@@ -8,10 +8,13 @@ import {
   useContract,
   useContractRead,
   useContractWrite,
-  useSigner
+  useSigner,
 } from "wagmi";
 
 import { useState } from "react";
+
+import { Card, Space, Input, Button, notification, Form } from "antd";
+import "antd/dist/antd.css";
 
 import deployedContracts from "../../contracts/hardhat_contracts.json";
 
@@ -24,77 +27,107 @@ export default function App() {
   let storageContract = {
     address: "",
     abi: [],
-  }
+  };
 
-  console.log(networkData.chain)
-  if(!loading) storageContract = getContractData(networkData?.chain, "Storage"); 
-  console.log(storageContract);
-  // let storageContract = getContractData(networkData?.chain, "Storage");
+  console.log(networkData.chain);
+  if (!loading)
+    storageContract = getContractData(networkData?.chain, "Storage");
 
   if (accountData) {
     return (
-      <div>
-        <div>
-          {accountData.ens?.name
-            ? `${accountData.ens?.name} (${accountData.address})`
-            : accountData.address}
-        </div>
-        <div>Connected to {networkData.chain?.name}</div>
-        <div>Connected to {accountData.connector.name}</div>
-        <button onClick={disconnect}>Disconnect</button>
-        {storageContract ? (
-          <Contract contract={storageContract} />
-        ) : (
-          <></>
-        )}
-
-      </div>
+      <Space wrap size="small">
+        <Card title="Connection Info">
+          <p>Account address: {accountData.address}</p>
+          <p>Connected to {networkData.chain?.name}</p>
+          <p>Connected via {accountData.connector.name}</p>
+          <Button onClick={disconnect}>Disconnect</Button>
+        </Card>
+        {storageContract ? <Contract contract={storageContract} /> : <></>}
+      </Space>
     );
   }
 
   return (
-    <>
-      <div>
-        {connectData.connectors.map((x) => (
-          <button key={x.id} onClick={() => connect(x)}>
-            {x.name}
-            {!x.ready && " (unsupported)"}
-          </button>
-        ))}
+    <Space>
+      {connectData.connectors.map((x) => (
+        <Button key={x.id} onClick={() => connect(x)}>
+          {x.name}
+          {!x.ready && " (unsupported)"}
+        </Button>
+      ))}
 
-        {connectError && <div>{connectError?.message ?? "Failed to connect"}</div>}
-      </div>
-    </>
+      {connectError && (
+        <div>{connectError?.message ?? "Failed to connect"}</div>
+      )}
+    </Space>
   );
 }
 
 const Contract = (props) => {
-  const [{ data, error, loading }, getSigner] = useSigner()
-  const [number, setNumber] = useState()
+  const [{ data, error, loading }, getSigner] = useSigner();
+  const [number, setNumber] = useState();
 
   let config = {
     addressOrName: props.contract.address,
     contractInterface: props.contract.abi,
-  }
+  };
 
-  const contract = useContract({...config, signerOrProvider: data})
+  const contract = useContract({ ...config, signerOrProvider: data });
 
   const retrieve = async () => {
-    setNumber((await contract.retrieve()).toString())
-  }
+    const number = (await contract.retrieve()).toString();
+    setNumber(number);
+    notification.open({
+      message: "Retrieved value",
+      description: `Contract storage retrieved: ${number}`,
+    });
+  };
+
+  const setStorage = async (values) => {
+    await contract.store(values.setStorage);
+    notification.open({
+      message: "Updating Storage",
+      description: `Contract storage updating to: ${values.setStorage}`,
+    });
+  };
 
   return (
-    <>
-      <p>Contract Address: {contract.address}</p>
-      <p>Contract number: {number}</p>
-      <button onClick={() => contract.store(5)}>Store 5</button>
-      <button onClick={retrieve}>Retrieve number</button>
-    </>
-  )
-}
+    <Space direction="vertical">
+      <Card title="Custom Contract">
+        <Space direction="vertical">
+          <p>Contract Address: {contract.address}</p>
+          <p>Contract number: {number}</p>
+          <Button onClick={retrieve}>Retrieve number</Button>
+          <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            initialValues={{ remember: true }}
+            onFinish={setStorage}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="New storage value"
+              name="setStorage"
+              rules={[
+                { required: true, message: "Please input new storage value" },
+              ]}
+            >
+              <Input placeholder="Set new storage value"></Input>
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Set Storage
+              </Button>
+            </Form.Item>
+          </Form>
+        </Space>
+      </Card>
+    </Space>
+  );
+};
 
 const getContractData = (network, name) => {
-  return deployedContracts[network?.id]?.[
-    network?.name?.toLocaleLowerCase()
-  ].contracts[name];
+  return deployedContracts[network?.id]?.[network?.name?.toLocaleLowerCase()]
+    .contracts[name];
 };
