567e5ef81d5b4385f4da9312a7684c446292eeda
Josh Crites <critesjosh@gmail.com>
2022-01-25 19:54:26 -0500

    add PWA

--- packages/react-app-wagmi/next.config.js
+++ packages/react-app-wagmi/next.config.js
@@ -1,3 +1,11 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
   reactStrictMode: true,
-}
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+});
