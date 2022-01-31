47d0b384b53fa0fde16b1479e0c25ef24a1155d0
Josh Crites <critesjosh@gmail.com>
2022-01-31 15:23:48 -0500

    add graph provider, comments

--- packages/react-app-wagmi/pages/_app.js
+++ packages/react-app-wagmi/pages/_app.js
@@ -1,20 +1,26 @@
 import "../styles/globals.css";
 import { Provider, Connector } from "wagmi";
+import { providers } from "ethers";
 import { Celo, Alfajores, Localhost } from "../utils/constants.ts";
 import { InjectedConnector } from "wagmi/connectors/injected";
 import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
 import Head from "next/head";
+import { ApolloProvider } from "@apollo/client";
+import client from "../apollo-client";
 
+// Define the Connectors for the DApp
 const connectors = [
   new InjectedConnector({ chains: [Celo, Alfajores, Localhost] }),
   new WalletConnectConnector({
-    chains: [Celo, Alfajores, Localhost],
+    chains: [Celo, Alfajores],
     options: {
       qrcode: true,
       rpc: {
-        1: "https://mainnet.infura.io/v3/feb93462d08c4db093c63c120aebcb55",
-        44787: "https://alfajores-forno.celo-testnet.org",
-        42220: "https://forno.celo.org",
+        // There is a bug where on the first connection the provider URL defaults to network id 1 (ETH mainnet).
+        // Set the RPC endpoint of your default network of choice as the #1
+        // 1: "https://alfajores-forno.celo-testnet.org",
+        44787: "https://alfajores-forno.celo-testnet.org", // Alfajores testnet
+        42220: "https://forno.celo.org", // Celo mainnet
       },
     },
   }),
@@ -28,7 +34,13 @@ function MyApp({ Component, pageProps }) {
         <meta name="description" content="Celo DApp Starter" />
       </Head>
       <Provider connectors={connectors}>
-        <Component {...pageProps} />
+        <ApolloProvider client={client}>
+          <div suppressHydrationWarning>
+            {typeof window === "undefined" ? null : (
+              <Component {...pageProps} />
+            )}
+          </div>
+        </ApolloProvider>
       </Provider>
     </>
   );
