9d0edf02ba662d54de5cd9a9c911dc32b730c20f
Josh Crites <critesjosh@gmail.com>
2022-02-04 17:08:49 -0500

    remove custom hooks

--- packages/react-app-usecontractkit/hooks/index.js
+++
@@ -1,4 +0,0 @@
-export { default as useLocalStorage } from "./useLocalStorage";
-export { default as useStaticJsonRPC } from "./useStaticJsonRPC";
-export * from "./useContractConfig";
-export * from './useContract';
