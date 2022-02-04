1609c366a466d6e11670facfcd581dd2fe5114e5
Josh Crites <critesjosh@gmail.com>
2022-02-04 17:10:08 -0500

    add head, format

--- packages/react-app-usecontractkit/pages/_app.tsx
+++ packages/react-app-usecontractkit/pages/_app.tsx
@@ -1,38 +1,49 @@
-import '@celo-tools/use-contractkit/lib/styles.css';
-import '../styles/globals.css';
+import "@celo-tools/use-contractkit/lib/styles.css";
+import "../styles/globals.css";
+import Head from "next/head";
 
-import { Alfajores, Localhost, ContractKitProvider } from '@celo-tools/use-contractkit';
-import { AppProps } from 'next/app';
-import { Toaster } from 'react-hot-toast';
+import {
+  Alfajores,
+  Mainnet,
+  ContractKitProvider,
+} from "@celo-tools/use-contractkit";
+import { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps, router }: AppProps): React.ReactElement {
-  if (router.route === '/wallet') {
+  if (router.route === "/wallet") {
     return <Component {...pageProps} />;
   }
 
   return (
-    <ContractKitProvider
-      dapp={{
-        name: 'use-contractkit demo',
-        description: 'A demo DApp to showcase functionality',
-        url: 'https://use-contractkit.vercel.app',
-        icon: 'https://use-contractkit.vercel.app/favicon.ico',
-      }}
-      network={Localhost}
-    >
-      <Toaster
-        position="top-right"
-        toastOptions={{
-          className: 'w-72 md:w-96',
-          style: {
-            padding: '0px',
-          },
+    <>
+      <Head>
+        <title>Celo DApp Starter</title>
+        <meta name="description" content="Celo DApp Starter" />
+      </Head>
+      <ContractKitProvider
+        dapp={{
+          name: "use-contractkit demo",
+          description: "A demo DApp to showcase functionality",
+          url: "https://use-contractkit.vercel.app",
+          icon: "https://use-contractkit.vercel.app/favicon.ico",
         }}
-      />
-      <div suppressHydrationWarning>
-        {typeof window === 'undefined' ? null : <Component {...pageProps} />}
-      </div>
-    </ContractKitProvider>
+        networks={[Alfajores, Mainnet]}
+      >
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            className: "w-72 md:w-96",
+            style: {
+              padding: "0px",
+            },
+          }}
+        />
+        <div suppressHydrationWarning>
+          {typeof window === "undefined" ? null : <Component {...pageProps} />}
+        </div>
+      </ContractKitProvider>
+    </>
   );
 }
 
