8501b4714d821f1d6dfcca90e05aaba3f553e387
Josh Crites <critesjosh@gmail.com>
2022-02-07 15:20:24 -0500

    remove wagmi dir

--- packages/nextjs-wagmi-ui/apollo-client.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const client = new ApolloClient({
-    uri: "https://api.thegraph.com/subgraphs/id/QmcBHXxQo2D89snhaud3fMhdbxTkj7YgZSAFeaMyrVS4oL",
-    cache: new InMemoryCache(),
-});
-
-export default client;
