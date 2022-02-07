ffaea940107966fe49e03cdab9f3be01e65654f7
Josh Crites <critesjosh@gmail.com>
2022-02-07 12:55:03 -0500

    add contract tabs

--- packages/react-app-usecontractkit/pages/index.tsx
+++ packages/react-app-usecontractkit/pages/index.tsx
@@ -1,37 +1,81 @@
-import { useState } from "react";
-
-import { Card, Button } from "antd";
-// import "antd/dist/antd.css";
+import * as React from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 import deployedContracts from "../../hardhat/deployments/hardhat_contracts.json";
 import { useContractKit } from "@celo-tools/use-contractkit";
 
 import { StorageContract, ButtonAppBar } from "../components";
 
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
 export default function App() {
-  const {
-    kit,
-    address,
-    network,
-  } = useContractKit();
+  const { network } = useContractKit();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
 
   const contracts =
     deployedContracts[network?.chainId?.toString()]?.[
       network?.name?.toLocaleLowerCase()
     ]?.contracts;
 
-  console.log(kit);
   return (
     <div>
-      <ButtonAppBar/>
-      {address && (
-        <>
-          <div>
-            {/* <GreeterContract contracts={contracts} /> */}
-            <StorageContract contractData={contracts?.Storage} />
-          </div>
-        </>
+      <ButtonAppBar />
+      <Box sx={{ width: "100%" }}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            aria-label="basic tabs example"
+          >
+            <Tab label="Storage Contract" {...a11yProps(0)} />
+            <Tab label="Greeter Contract" {...a11yProps(1)} />
+          </Tabs>
+        </Box>
+        <TabPanel value={value} index={0}>
+          <StorageContract contractData={contracts?.Storage} />
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          {/* <GreeterContract contracts={contracts} /> */}
+        </TabPanel>
+      </Box>
+    </div>
+  );
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
       )}
     </div>
   );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
 }
