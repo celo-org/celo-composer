e202997d7fa8952704896b8876eb42c348793926
Josh Crites <critesjosh@gmail.com>
2022-02-07 13:19:24 -0500

    add pwa

--- packages/react-app-usecontractkit/next.config.js
+++ packages/react-app-usecontractkit/next.config.js
@@ -1,5 +1,13 @@
-module.exports = {
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
   reactStrictMode: true,
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
@@ -10,4 +18,4 @@ module.exports = {
     };
     return config;
   },
-}
+})
