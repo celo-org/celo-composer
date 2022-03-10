e149f1445294a27cf7f0acfd72bd173d67506e8f
LAP14205 <lap14205@LAP14205s-MacBook-Pro.local>
2022-03-10 18:43:16 +0700

    improve UI

--- packages/react-app/components/ButtonAppBar.tsx
+++ packages/react-app/components/ButtonAppBar.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -12,11 +12,6 @@ import { truncateAddress, getWindowDimensions } from '@/utils'
 export function ButtonAppBar() {
   const { address, network, connect, destroy } = useContractKit();
   const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-
-  useEffect(() => {
-    console.log("----")
-    console.log(windowDimensions)
-  }, []);
 
   return (
     <Box sx={{ flexGrow: 1 }}>
