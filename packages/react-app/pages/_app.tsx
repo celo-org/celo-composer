654944cd7418b19850862a57e8d7ac38d3143f5b
Charlie Rabiller <charlie.rabiller@loreal.com>
2022-03-28 12:06:49 -0400

    add switch to change dark/light mode

--- packages/react-app/pages/_app.tsx
+++ packages/react-app/pages/_app.tsx
@@ -1,42 +1,42 @@
 import React from "react";
 import "@celo-tools/use-contractkit/lib/styles.css";
-import Head from "next/head";
 import { SnackbarProvider } from "notistack";
 import { ApolloProvider } from "@apollo/client";
 import client from "@/apollo-client";
 import { Alfajores, ContractKitProvider } from "@celo-tools/use-contractkit";
 import { AppProps } from "next/app";
-import Footer from "@/components/layout/Footer";
-import Meta from "@/components/meta/Meta";
+import { CustomThemeProvider } from "@/contexts/UserTheme";
 
 function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
   return (
-    <ContractKitProvider
-      dapp={{
-        name: "use-contractkit demo",
-        description: "A demo DApp to showcase functionality",
-        url: "https://use-contractkit.vercel.app",
-        icon: "https://use-contractkit.vercel.app/favicon.ico",
-      }}
-      network={Alfajores}
-      // networks={[Mainnet, Alfajores]}
-    >
-      <SnackbarProvider
-        maxSnack={3}
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "right",
+    <CustomThemeProvider>
+      <ContractKitProvider
+        dapp={{
+          name: "use-contractkit demo",
+          description: "A demo DApp to showcase functionality",
+          url: "https://use-contractkit.vercel.app",
+          icon: "https://use-contractkit.vercel.app/favicon.ico",
         }}
+        network={Alfajores}
+        // networks={[Mainnet, Alfajores]}
       >
-        <ApolloProvider client={client}>
-          <div suppressHydrationWarning>
-            {typeof window === "undefined" ? null : (
-              <Component {...pageProps} />
-            )}
-          </div>
-        </ApolloProvider>
-      </SnackbarProvider>
-    </ContractKitProvider>
+        <SnackbarProvider
+          maxSnack={3}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "right",
+          }}
+        >
+          <ApolloProvider client={client}>
+            <div suppressHydrationWarning>
+              {typeof window === "undefined" ? null : (
+                <Component {...pageProps} />
+              )}
+            </div>
+          </ApolloProvider>
+        </SnackbarProvider>
+      </ContractKitProvider>
+    </CustomThemeProvider>
   );
 }
 
