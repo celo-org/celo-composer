27901aeab0f733bc14d9f560185f769db52397bb
hatskier <suvorovalex1997@gmail.com>
2022-03-30 14:17:45 +0200

    added redstone oracles example

---
+++ packages/hardhat/test/oracle-test.js
@@ -0,0 +1,24 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { WrapperBuilder } = require("redstone-evm-connector");
+
+// You can find more details about RedStone oracles here: https://tinyurl.com/redstone-celo-docs
+
+describe("Oracle", function () {
+  it("Should correctly calculate CELO tokens amount for $100", async function () {
+    const Oracle = await ethers.getContractFactory("Oracle");
+    const oracle = await Oracle.deploy();
+    await oracle.deployed();
+
+    const usdAmount = ethers.utils.parseEther("100");
+    const wrappedContract = WrapperBuilder
+      .wrapLite(oracle)
+      .usingPriceFeed("redstone", { asset: "CELO" });
+    const celoAmount = await wrappedContract.getCELOAmountForUSDAmount(usdAmount);
+
+    expect(celoAmount).to.be.lessThan(100); // It means that CELO price is higher than $1
+
+    // Print celo amount
+    console.log(`Celo amount for $100: ${celoAmount.toNumber() / 10 ** 8}`);
+  });
+});
