aef4e497a97029f28c3e4b3a8c9f977e9cc6c78d
Rene Aavik <renx81@gmail.com>
2022-08-14 18:19:54 +0300

    Fix error caused by merge conflict

--- packages/react-app/components/AccountInfo.tsx
+++ packages/react-app/components/AccountInfo.tsx
@@ -73,10 +73,11 @@ export function AccountInfo() {
       baseCurrency === BaseCurrency.USD
         ? 1
         : baseCurrency === BaseCurrency.EUR
-        ? 1.0 / eurUsdPrice.value
-        : baseCurrency === BaseCurrency.ETH
-        ? 1.0 / ethUsdPrice.value
-        : 1.0 / celoUsdPrice.value;
+          ? 1.0 / eurUsdPrice.value
+          : baseCurrency === BaseCurrency.ETH
+            ? 1.0 / ethUsdPrice.value
+            : 1.0 / celoUsdPrice.value
+    );
 
     setBalance({
       CELO: {
