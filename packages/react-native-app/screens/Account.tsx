8adb98fb618623ced7d16ea63ea61eb3c18ec160
Harpal Jadeja <jadejaharpal14@gmail.com>
2022-12-09 00:05:16 +0530

    chore: refactor add `AccountBalance`

--- packages/react-native-app/screens/Account.tsx
+++ packages/react-native-app/screens/Account.tsx
@@ -2,11 +2,12 @@ import { useState, useEffect, useContext } from "react";
 import { StyleSheet, ActivityIndicator, TextInput } from "react-native";
 import { useWalletConnect } from "@walletconnect/react-native-dapp";
 import { Text, View } from "../components/Themed";
-import { Button } from "../components/Button";
+import Button from "../components/Button";
 import * as WebBrowser from "expo-web-browser";
 import Web3 from "web3";
 import { ThemeContext } from "../context/ThemeProvider";
 import AccountAddress from "../components/AccountAddress";
+import AccountBalance from "../components/AccountBalance";
 
 export default function Account() {
     const connector = useWalletConnect();
@@ -26,6 +27,7 @@ export default function Account() {
                 <Text style={styles.title}>Account Info</Text>
                 <Button style={styles.externalLink} onPress={handlePress}>
                     <AccountAddress />
+                    <AccountBalance />
                 </Button>
             </View>
             <View style={styles.separator}></View>
