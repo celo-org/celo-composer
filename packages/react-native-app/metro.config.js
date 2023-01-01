8ea4872427541a3a0c44ad5e0bda0d0bd0fa6165
Harpal Jadeja <jadejaharpal14@gmail.com>
2023-01-01 14:39:36 +0530

    chore: remove react-native-tcp dependency

--- packages/react-native-app/metro.config.js
+++ packages/react-native-app/metro.config.js
@@ -14,24 +14,24 @@ const config = getDefaultConfig(projectRoot);
 config.watchFolders = [workspaceRoot];
 // 2. Let Metro know where to resolve packages, and in what order
 config.resolver.nodeModulesPaths = [
-	path.resolve(projectRoot, "node_modules"),
-	path.resolve(workspaceRoot, "node_modules"),
+    path.resolve(projectRoot, "node_modules"),
+    path.resolve(workspaceRoot, "node_modules"),
 ];
 
 module.exports = {
-	...config,
-	resolver: {
-		extraNodeModules: {
-			crypto,
-			url,
-			fs: require.resolve("expo-file-system"),
-			http: require.resolve("stream-http"),
-			https: require.resolve("https-browserify"),
-			net: require.resolve("react-native-tcp"),
-			os: require.resolve("os-browserify/browser.js"),
-			path: require.resolve("path-browserify"),
-			stream: require.resolve("readable-stream"),
-			vm: require.resolve("vm-browserify"),
-		},
-	},
+    ...config,
+    resolver: {
+        extraNodeModules: {
+            crypto,
+            url,
+            fs: require.resolve("expo-file-system"),
+            http: require.resolve("stream-http"),
+            https: require.resolve("https-browserify"),
+            // net: require.resolve("react-native-tcp"),
+            os: require.resolve("os-browserify/browser.js"),
+            path: require.resolve("path-browserify"),
+            stream: require.resolve("readable-stream"),
+            vm: require.resolve("vm-browserify"),
+        },
+    },
 };
