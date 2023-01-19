40d3c6aa0927f6f1ddd9d8056a248e9ab7c37470
Nestor Bonilla <nestor.bonilla.s@gmail.com>
2023-01-19 14:44:35 -0800

    adding rebranding updates to react-app

--- packages/react-app/components/Footer.tsx
+++ packages/react-app/components/Footer.tsx
@@ -30,18 +30,18 @@ const navigation = [
   
   export default function Footer() {
     return (
-      <footer className="bg-white mt-auto">
-        <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+      <footer className="bg-gypsum mt-auto border-black border-t">
+        <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
           <div className="flex justify-center space-x-6 md:order-2">
             {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500" target="_blank" rel="noopener noreferrer">
+              <a key={item.name} href={item.href} className="text-black hover:text-forest" target="_blank" rel="noopener noreferrer">
                 <span className="sr-only">{item.name}</span>
                 <item.icon className="h-6 w-6" aria-hidden="true" />
               </a>
             ))}
           </div>
           <div className="mt-8 md:order-1 md:mt-0">
-            <p className="text-center text-base text-gray-400">&copy; {new Date().getFullYear()} Developed with 💚 by Celo DevRel team.</p>
+            <p className="text-center text-base text-black">&copy; {new Date().getFullYear()} Developed with 🖤 by the Celo DevRel team.</p>
           </div>
         </div>
       </footer>
--- packages/react-app/components/Layout.tsx
+++ packages/react-app/components/Layout.tsx
@@ -8,7 +8,7 @@ interface Props {
 const Layout: FC<Props> = ({children}) => {
     return (
         <>
-            <div className="bg-gray-50 overflow-hidden flex flex-col min-h-screen">
+            <div className="bg-gypsum overflow-hidden flex flex-col min-h-screen">
             <Header />
                 <div className="py-16 max-w-7xl mx-auto space-y-8 sm:px-6 lg:px-8">
                     {children}
