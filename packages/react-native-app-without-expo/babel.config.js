89029314ca79ad70a6bf80b12ee7f2b47f1f5695
Harpal Jadeja <jadejaharpal14@gmail.com>
2023-06-19 16:06:43 +0530

    fix: `React` not found issue

--- packages/react-native-app-without-expo/babel.config.js
+++ packages/react-native-app-without-expo/babel.config.js
@@ -1,3 +1,27 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          envName: 'APP_ENV',
+          moduleName: '@env',
+          path: '.env',
+          blocklist: null,
+          allowlist: null,
+          safe: false,
+          allowUndefined: true,
+          verbose: false,
+        },
+      ],
+      [
+        '@babel/plugin-transform-react-jsx',
+        {
+          runtime: 'automatic',
+        },
+      ],
+    ],
+  };
 };
