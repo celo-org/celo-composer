b10bb1b1af91660fac1fed89b8b0f23eceee7d38
Viral Sangani <viral.sangani2011@gmail.com>
2023-09-17 09:33:44 +0200

    Support for minipay added

--- packages/react-app/pages/index.tsx
+++ packages/react-app/pages/index.tsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+import { useAccount } from "wagmi";
+
 export default function Home() {
+  const [userAddress, setUserAddress] = useState("");
+  const { address, isConnected } = useAccount();
+
+  useEffect(() => {
+    if (isConnected && address) {
+      setUserAddress(address);
+    }
+  }, [address, isConnected]);
+
   return (
-    <div>
-      <div className="h1">There you go... a canvas for your next Celo project!</div>
+    <div className="flex flex-col justify-center items-center">
+      <div className="h1">
+        There you go... a canvas for your next Celo project!
+      </div>
+      {isConnected && (
+        <div className="h2 text-center">Your address: {userAddress}</div>
+      )}
     </div>
-  )
+  );
 }
