1862ccb670770f95ea0e45e7751acc0546b61391
Viral Sangani <viral.sangani2011@gmail.com>
2023-09-18 21:42:29 +0200

    Support for minipay added (#217)

--- packages/react-app/pages/_app.tsx
+++ packages/react-app/pages/_app.tsx
@@ -1,51 +1,45 @@
-import type { AppProps } from "next/app";
-import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { Alfajores, Celo } from "@celo/rainbowkit-celo/chains";
 import celoGroups from "@celo/rainbowkit-celo/lists";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
+import type { AppProps } from "next/app";
+import { WagmiConfig, configureChains, createConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
-import "@rainbow-me/rainbowkit/styles.css";
-import { publicProvider } from "wagmi/providers/public";
-import { Alfajores, Celo } from "@celo/rainbowkit-celo/chains";
 
 const projectId = process.env.NEXT_PUBLIC_WC_PROJECT_ID as string; // get one at https://cloud.walletconnect.com/app
 
 const { chains, publicClient } = configureChains(
-    [Celo, Alfajores],
-    [publicProvider()]
+  [Celo, Alfajores],
+  [publicProvider()]
 );
 
 const connectors = celoGroups({
-    chains,
-    projectId,
-    appName:
-        (typeof document === "object" && document.title) || "Your App Name",
+  chains,
+  projectId,
+  appName: (typeof document === "object" && document.title) || "Your App Name",
 });
 
 const appInfo = {
-    appName: "Celo Composer",
+  appName: "Celo Composer",
 };
 
 const wagmiConfig = createConfig({
-    autoConnect: true,
-    connectors,
-    publicClient: publicClient,
+  connectors,
+  publicClient: publicClient,
 });
 
 function App({ Component, pageProps }: AppProps) {
-    return (
-        <WagmiConfig config={wagmiConfig}>
-            <RainbowKitProvider
-                chains={chains}
-                appInfo={appInfo}
-                coolMode={true}
-            >
-                <Layout>
-                    <Component {...pageProps} />
-                </Layout>
-            </RainbowKitProvider>
-        </WagmiConfig>
-    );
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains} appInfo={appInfo} coolMode={true}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
 
 export default App;
